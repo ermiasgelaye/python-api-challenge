3c0dafdfea488d9257e9633f7f079fd52ae34b59
Ermias Gaga <ermiasgelaye@gmail.com>
2020-06-23 02:42:19 -0400

    Update api_keys.py

--- WeatherPy/api_keys.py
+++ WeatherPy/api_keys.py
@@ -1,5 +1,5 @@
 # OpenWeatherMap API Key
-weather_api_key = "cf1ae81b5b7228c0d188aa3a88a12517"
+weather_api_key = "API_KEY_HERE"
 
 # Google API Key
-g_key = "AIzaSyBgbuKNnsZQfo-IfZsrC70_giE5ZDJHn9U"
+g_key = "API_KEY_HERE"
