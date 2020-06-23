40cc92b6a8af2836ab9408885884355710a16a6e
Ermias Gaga <ermiasgelaye@gmail.com>
2020-06-23 02:41:25 -0400

    Update api_keys.py

--- VacationPy/api_keys.py
+++ VacationPy/api_keys.py
@@ -1,5 +1,5 @@
 # OpenWeatherMap API Key
-weather_api_key = "cf1ae81b5b7228c0d188aa3a88a12517"
+weather_api_key = "API_KEY_HERE"
 
 # Google API Key
-g_key = "AIzaSyAibs0RrCQcUXfgVabsw6fLBWJsPHiRMXo"
+g_key = "API_KEY_HERE"
